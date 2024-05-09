45b80cfbd57e20c1f3b2fc7f844ff2eea68a28df
Tapia Aguirre Jeferson Vinicio <jvtapia@banecuador.fin.ec>
2024-05-09 11:37:18 -0500

    segundo aporte

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -16,11 +16,12 @@ export class UsersService {
     return `This action returns a #${id} user`;
   }
 
+ 
+ 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+ 
+ 
 }
